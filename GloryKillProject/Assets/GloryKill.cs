325b741ada9afb17ad17d472a8ac5cec937d1cfc
Supernova1114 <supernovawarriors128@gmail.com>
2020-08-28 21:40:14 -0700

    Added ghost and added ghost glory kill

---
+++ GloryKillProject/Assets/GloryKill.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloryKill : MonoBehaviour
+{
+    private RaycastHit2D hit;
+    [SerializeField]
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+            hit = Physics2D.Raycast(transform.position, Vector2.right, 2);
+            
+            if (hit.collider != null)
+            {
+                print(hit.collider.gameObject.name);
+                if (hit.collider.CompareTag("ghostStag"))
+                {
+                    StartCoroutine(PlayGlory());
+                }
+            }
+        }
+    }
+
+    private IEnumerator PlayGlory()
+    {
+        while ( (hit.collider.transform.position - transform.position).magnitude > 1.5)
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+        print("play animation");
+
+        Destroy(hit.collider.gameObject, 0);
+        animator.SetBool("Ghost", true);
+        yield return new WaitForSeconds(0.5f);
+        animator.SetBool("Ghost", false);
+
+    }
+
+}
---
+++ GloryKillProject/Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontalRaw;
+    private float horizontal;
+
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private Rigidbody2D body;
+    [SerializeField]
+    private float maxVelocity;
+    [SerializeField]
+    private float hMag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalRaw = Input.GetAxisRaw("Horizontal");//0 or 1
+        horizontal = Input.GetAxis("Horizontal");
+
+
+        animator.SetFloat("Horizontal", Math.Abs(horizontal));
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move(horizontalRaw);
+
+
+    }
+
+    private void Move(float horzRaw)
+    {
+        
+
+        body.AddForce(new Vector2(horzRaw * hMag, 0));
+        body.velocity = Vector2.ClampMagnitude(body.velocity, maxVelocity);
+        //print(body.velocity.x);
+
+    }
+
+}
