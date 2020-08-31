b0e2b62b8dfbbd0e0b9c6f27b28776464ae537fc
Supernova1114 <supernovawarriors128@gmail.com>
2020-08-31 13:06:13 -0700

    Started adding ghost transitions

---
+++ GloryKillProject/Assets/GhostAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostAI : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    private bool flag = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && flag)
+        {
+            flag = false;
+            animator.SetTrigger("MakeAngry");
+            yield return new WaitForSeconds(2);
+            animator.ResetTrigger("MakeAngry");
+
+            animator.SetTrigger("Stagger");
+            yield return new WaitForSeconds(2);
+            animator.ResetTrigger("Stagger");
+            flag = true;//needs change
+        }
+    }
+
+}
