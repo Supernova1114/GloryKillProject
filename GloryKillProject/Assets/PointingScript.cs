e83ed58cd3abde52ce94d6510367561a0ef60966
Supernova1114 <supernovawarriors128@gmail.com>
2020-09-05 00:07:25 -0700

    made outline shader, redid roatation
    rotation still need to be fixed for flipping  character

--- GloryKillProject/Assets/PointingScript.cs
+++ GloryKillProject/Assets/PointingScript.cs
@@ -11,10 +11,12 @@ public class PointingScript : MonoBehaviour
     Vector2 currentVelocity;
     public float maxSpeed;
 
+    PlayerMovement movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement = GetComponentInParent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -25,23 +27,36 @@ public class PointingScript : MonoBehaviour
         transform.up = (mousePos - transform.position)*/
         currentVelocity = body.velocity;
 
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        Vector2 direction = new Vector2(
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        /*Vector2 direction = new Vector2(
         mousePosition.x - transform.position.x,
         mousePosition.y - transform.position.y
-        );
+        );*/
+
+        Vector2 slopeVect = (mousePosition - (Vector2)transform.position);
 
         if (!PlayerMovement.isWalking())
         {
-            transform.up = Vector2.SmoothDamp(transform.up, direction, ref currentVelocity, smoothTime, maxSpeed);
+            slopeVect = Vector2.SmoothDamp(transform.right, slopeVect, ref currentVelocity, smoothTime, maxSpeed);
         }
         else
         {
-            transform.up = Vector2.SmoothDamp(transform.up, transform.localPosition + Vector3.down, ref currentVelocity, 0.03f, maxSpeed);
+            slopeVect = Vector2.SmoothDamp(transform.right, transform.localPosition + Vector3.down, ref currentVelocity, 0.03f, maxSpeed);
         }
 
-        //transform.up = direction;
+
+        float rotation = Mathf.Rad2Deg * Mathf.Atan2(slopeVect.y,slopeVect.x);
+
+        print(movement.gameObject.transform.rotation.eulerAngles.y);
+
+
+
+        transform.rotation = Quaternion.Euler(0, movement.gameObject.transform.rotation.eulerAngles.y, rotation);
+
+
+        
+
+        
 
     }
 }
