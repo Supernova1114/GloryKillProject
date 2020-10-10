97a1edc4a2809f0c1688674c74f2352684f1644e
Supernova1114 <supernovawarriors128@gmail.com>
2020-10-10 13:21:28 -0700

    meswsed with cinemachine

--- GloryKillProject/Assets/SizeController.cs
+++ GloryKillProject/Assets/SizeController.cs
@@ -6,21 +6,23 @@ using UnityEngine;
 
 public class SizeController : MonoBehaviour
 {
-    public CinemachineTargetGroup.Target[] targetGroup;
+    public CinemachineVirtualCamera cam;
+    public CinemachineTargetGroup group;
     public GameObject rollerBug;
 
     // Start is called before the first frame update
     void Start()
     {
         //ArrayList ghosts = new ArrayList();
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        group.AddMember(rollerBug.transform, 1, 0);
 
         //targetGroup[0] = enemies[0];
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+       // if (group.BoundingBox != null)
+        //cam.m_Lens.OrthographicSize = Camera.current.WorldToViewportPoint( group.BoundingBox.size ).magnitude;
     }
 }
