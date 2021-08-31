013349328f128636ff5d0996d705d7aea5144bd4
Supernova1114 <supernovawarriors128@gmail.com>
2021-08-31 11:43:58 -0700

    Added a Sprite Shape

---
+++ GloryKillProject/Assets/RollerBugNeckController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerBugNeckController : MonoBehaviour
+{
+    public UnityEngine.U2D.SpriteShapeController spriteShapeController;
+    private UnityEngine.U2D.Spline spline;
+    public GameObject rollerBug;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteShapeController = GetComponent<UnityEngine.U2D.SpriteShapeController>();
+        spline = spriteShapeController.spline;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spline.SetPosition(0, rollerBug.transform.position);
+    }
+}
